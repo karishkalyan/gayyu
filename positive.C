4a90654afe2701d1010a0dc21743238b269199cd
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-01-10 16:39:06 +0530

    positive
    Positive or negative

---
+++ positive.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<conio.h>
+void main(void) 
+{
+	intx,y;
+	printf("\n enter the number");
+	scanf('%d',&x);
+	if(x==0)
+	{
+		printf("\n the given number is zero ");
+	}
+		else
+		{
+			if(x<0)
+			printf("\n the given number is negative");
+			else
+			printf("\n the given number is positive")
+		}
+	
+
+	
+	return 0;
+}
